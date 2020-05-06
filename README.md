c99708cfaaaddfde529c9773af40c8edd2992353
Allan B. Santos <allan.itcom@gmail.com>
2020-05-05 21:02:28 -0300

    Module Test

---
+++ README.md
@@ -0,0 +1,1 @@
+It is a simple module that creates a menu named TechNimbus on the Magento admin dashboard to quick jump on the System Settings of all TechNimbus modules
---
+++ registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'TechNimbus_MainMenu', __DIR__);
